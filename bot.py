60a08ee1e441f651406007d4b67dfe8b88881157
habib kazemi <kazemihabib1996@gmail.com>
2016-11-01 02:13:23 +0330

    add inline keyboard

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater, CommandHandler,Filters ,MessageHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import api
 
 id_storage = {}
@@ -20,49 +21,51 @@ def  checkMessageType(bot,update):
         findLyrics(bot,update)
 
 def sendLyric(bot,update):
-    #******************************************************
     global id_storage    
     chatId = update.message.chat_id
-    #******************************************************
 
     choice = int(update.message.text)
 
-    #******************************************************
     if chatId in id_storage:
-    #******************************************************
         try:
             links = id_storage[chatId] 
             link = links[choice] 
 
             lyric = api.lyric(link)
             update.message.reply_text(str(lyric))
-    #******************************************************
         except IndexError as indexerror:
             print('IndexError: ',indexerror)
             update.message.reply_text("Oops!\n No previous song with this number is found.")
 
-    #******************************************************
         except Exception as error:
             print('error: ',error)
             update.message.reply_text("Oops! i\'m sorry!\n Something went wrong.")
             
 def findLyrics(bot,update):
-    #**************************************
     global id_storage
     links = api.search(update.message.text)
     id_storage[update.message.chat_id]=links
-    #**************************************
 
     lyrics=[]
     for index,link in enumerate(links):
         lyric = link.replace('http://www.metrolyrics.com/','').replace('.html','').replace('-lyrics-',' ').replace('-', ' ')
         lyrics.append(str(index)+' - ' + lyric)
       
-    lyrics = '\n'.join(lyrics)
-    update.message.reply_text(lyrics)
+    # update.message.reply_text(lyrics)
+    #****************************************************
+    sendInlineKeyBoard(bot,update,lyrics) 
+    #****************************************************
 
-    print('links',links)
+#********************************************************
+def sendInlineKeyBoard(bot,update,lyrics):
+    chatId = update.message.chat_id
+    keyboard = [[InlineKeyboardButton(text, callback_data=str(index))] for index,text in enumerate(lyrics)] 
 
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    bot.sendMessage(chat_id=chatId,text='please choose:',reply_markup=reply_markup)
+
+#********************************************************
 def main():
     # Create the EventHandler and pass it your bot's token.
     updater = Updater("266267518:AAFW2y2drcXVaJr0NgIlA6nYwgzSWohvepo")
@@ -73,7 +76,8 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
 
-    dp.add_handler(MessageHandler(Filters.text,checkMessageType))
+    dp.add_handler(MessageHandler(Filters.text,findLyrics))
+    # dp.add_handler(CallbackQueryHandler(sendLyric))
 
     # log all errors
     dp.add_error_handler(error)
