15a7f900a5d817b9c6fb09319c6dffb61e924576
habib kazemi <kazemihabib1996@gmail.com>
2016-10-31 22:34:21 +0330

    first commit of '/start' bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from telegram.ext import Updater, CommandHandler 
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("266267518:AAFW2y2drcXVaJr0NgIlA6nYwgzSWohvepo")
+
+
+
+if __name__ == '__main__':
+    main()
